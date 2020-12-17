5692c7c7fecc6cbda4095a07d3a3832e337f0acb
César León <cesar.leon@devsu.com>
2020-12-17 14:14:46 -0500

    Update eslintignore and prettierignore

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,31 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          light: "#F8FBFC",
+          DEFAULT: "#0C3248",
+          dark: "#617A88",
+        },
+        secondary: {
+          DEFAULT: "#A4C22C",
+          gray: "#555555",
+          lightGray: "#EAEAEA",
+        },
+      },
+      boxShadow: {
+        card: "0 0 34px 0 rgba(0,0,0,.08)",
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      borderWidth: ["last"],
+    },
   },
   plugins: [],
 };

daea85786df8c607289da383b98e20de9e610413
César León <cesar.leon@devsu.com>
2021-02-26 20:38:36 -0500

    Added size (M or L) to each repeatable text section. Fixes on dots3 responsive sizing

--- components/slices/imageAndText.js
+++ components/slices/imageAndText.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { RichText } from "prismic-reactjs";
 import Button from "./../common/button";
 import ResponsiveImage from "../common/responsiveImage";
+import TextUtils from "../../utils/text";
 
 const STYLE = {
   LIGHT: "light",
@@ -16,6 +17,10 @@ const IMAGE_ALIGNMENT = {
 const IMAGE_SIZE = {
   MEDIUM: "medium",
   LARGE: "large",
+};
+
+const TITLE_SIZE = {
+  MEDIUM: "medium",
 };
 
 class ImageAndText extends Component {
@@ -53,6 +58,16 @@ class ImageAndText extends Component {
     return imageSize === IMAGE_SIZE.MEDIUM ? "w-2/3" : "w-1/2";
   }
 
+  getTextStyle(fontSize) {
+    return fontSize === TITLE_SIZE.MEDIUM
+      ? "text-sm pb-7 pt-2"
+      : "p_leading-loose li_leading-loose text-base lg:text-lg p_mb-10";
+  }
+
+  getTitleStyle(fontSize) {
+    return fontSize === TITLE_SIZE.MEDIUM ? "text-base" : "text-3xl pb-4 md:text-4xl lg:pb-8";
+  }
+
   render() {
     const { primary } = this.props.slice;
     const {
@@ -75,13 +90,13 @@ class ImageAndText extends Component {
         <div
           className={`overflow-hidden flex flex-col container mx-auto w-full px-6 md:px-14 lg:px-28 pb-12 ${flexStyles} md:pt-20 lg:py-28`}
         >
-          <div className={`py-10 md:py-0 md:${imageWidth} px-4 h-auto lg:px-12`}>
+          <div className={`py-10 md:py-0 md:${imageWidth} px-4 h-auto`}>
             <ResponsiveImage
               image={image}
               sizes="(min-width:1536) 648px, (min-width:768) 40vw, 75vw"
             />
           </div>
-          <div className={`pb-8 md:${textWidth} px-4`}>
+          <div className={`pb-8 md:${textWidth} px-4 md:px-12`}>
             <div className={`${titleColor} text-xs uppercase mb-8`}>
               {RichText.render(smallTitle)}
             </div>
@@ -103,16 +118,21 @@ class ImageAndText extends Component {
     const textColor = this.getTextColor(primary.style);
     const listClass = primary.list_columns === "2" ? "two-column-bullets" : "custom-line-bullets";
     return items.map((section, index) => {
+      const { font_size: fontSize, big_title: bigTitle, rich_text: richText } = section;
+      const textStyle = this.getTextStyle(fontSize);
+      const titleStyle = this.getTitleStyle(fontSize);
       return (
         <div key={index}>
-          <div className={`${titleColor} text-3xl font-bold pb-4 md:text-4xl lg:pb-8`}>
-            {RichText.render(section.big_title)}
-          </div>
-          <div
-            className={`${textColor} ${listClass} p_mb-10 p_leading-loose li_leading-loose text-base lg:text-lg`}
-          >
-            {RichText.render(section.rich_text)}
-          </div>
+          {TextUtils.hasRichText(bigTitle) && (
+            <div className={`${titleColor} ${titleStyle} font-bold`}>
+              {RichText.render(bigTitle)}
+            </div>
+          )}
+          {TextUtils.hasRichText(richText) && (
+            <div className={`${textColor} ${listClass} ${textStyle}`}>
+              {RichText.render(richText)}
+            </div>
+          )}
         </div>
       );
     });

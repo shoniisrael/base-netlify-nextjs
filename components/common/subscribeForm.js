f4b7f3b4d2d1f005518895f54b1d36691811ef08
César León <cesar.leon@devsu.com>
2021-03-23 12:25:15 -0500

    Add subscribe message

--- components/common/subscribeForm.js
+++ components/common/subscribeForm.js
@@ -4,7 +4,13 @@ import { useGoogleReCaptcha, GoogleReCaptcha } from "react-google-recaptcha-v3";
 const SubscribeForm = (props) => {
   const { executeRecaptcha } = useGoogleReCaptcha();
   const [disableSubmit, setDisableSubmit] = useState(false);
+  const [email, setEmail] = useState("");
+  const [submitted, setSubmitted] = useState(false);
 
+  const handleInputChange = (e) => {
+    setDisableSubmit(false);
+    setEmail(e.target.value);
+  };
   const submitData = async (token, data) => {
     const url = `/.netlify/functions/verify-captcha?token=${token}`;
     const fetchUrl = "/";
@@ -13,17 +19,21 @@ const SubscribeForm = (props) => {
       const response = await fetch(url);
       if (response.ok) {
         let urlEncodedData = "";
+        setEmail;
         urlEncodedData = data.join("&").replace(/%20/g, "+");
         fetch(fetchUrl, {
           method: "POST",
           headers: { "Content-Type": "application/x-www-form-urlencoded" },
           body: urlEncodedData,
+        }).then(() => {
+          setSubmitted(true);
+          setEmail("");
+          setDisableSubmit(false);
         });
       } else {
         /*global alert*/
         alert("Error: Please Try Again!");
       }
-      setDisableSubmit(false);
     } catch (err) {
       alert("Error: Please Try Again!");
       console.error(err);
@@ -40,24 +50,37 @@ const SubscribeForm = (props) => {
       );
     }
     const result = await executeRecaptcha("subscribe");
+    setEmail("");
     submitData(result, urlEncodedDataPairs);
   };
 
-  const { classes } = props;
+  const { classes, identifier = "" } = props;
   return (
-    <form netlify id="subscribe" method="post" onSubmit={onSubmit}>
-      <input type="hidden" name="form-name" value="subscribe" />
-      <input type="hidden" name="formType" value="subscribe" />
-      <input className={classes} type="email" name="email" placeholder="Your email" />
-      <input
-        className="g-recaptcha btn cursor-pointer filled md:text-sm lg:text-base whitespace-nowrap"
-        disabled={disableSubmit}
-        type="submit"
-        value="Subscribe"
-      />
-
-      <GoogleReCaptcha action={"subscribe"} />
-    </form>
+    <div>
+      <form netlify id={`subscribe-${identifier}`} method="post" onSubmit={onSubmit}>
+        <input type="hidden" name="form-name" value="subscribe" />
+        <input type="hidden" name="formType" value="subscribe" />
+        <input
+          className={classes}
+          type="email"
+          value={email}
+          onChange={handleInputChange}
+          name="email"
+          placeholder="Your email"
+          required
+        />
+        <input
+          className="g-recaptcha btn cursor-pointer filled md:text-sm lg:text-base whitespace-nowrap"
+          disabled={disableSubmit}
+          type="submit"
+          value="Subscribe"
+        />
+        <GoogleReCaptcha action="subscribe" />
+      </form>
+      <div className={submitted ? "p-2 mt-4 border-2 border-secondary" : "hidden"}>
+        THANK YOU FOR SUBSCRIBING
+      </div>
+    </div>
   );
 };
 export default SubscribeForm;

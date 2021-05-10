9404f6a1361f8bbd71ac88f37569b2926d4154e9
Eduardo Lima G <eduarditolima@gmail.com>
2021-05-10 08:49:40 -0500

    redirectURL home, inside preview

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -2,14 +2,16 @@ import { Client, linkResolver } from "../../prismic-configuration";
 
 export default async (req, res) => {
   const { token: ref, documentId } = req.query;
-
-  const redirectUrl = await Client(req)
+  let redirectUrl = await Client(req)
     .getPreviewResolver(ref, documentId)
-
     .resolve(linkResolver, "/");
 
   if (!redirectUrl) {
     return res.status(401).json({ message: "Invalid token" });
+  }
+
+  if (redirectUrl === "/") {
+    redirectUrl = "/home";
   }
 
   res.setPreviewData({ ref });

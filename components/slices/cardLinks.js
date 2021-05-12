68f48a97ac4b926a16e6db18172044b8cbffd20a
Eduardo Lima G <eduarditolima@gmail.com>
2021-05-12 15:28:16 -0500

    Add background style

--- components/slices/cardLinks.js
+++ components/slices/cardLinks.js
@@ -16,28 +16,33 @@ class CardLinks extends Component {
       title,
       subtitle,
       has_separation_line: hasSeparationLine,
-      background_style: backgroundStyle,
+      background_header_style: backgroundHeaderStyle,
+      background_header_color: backgroundHeaderColor,
+      background_body_style: backgroundBodyStyle,
+      background_body_color: backgroundBodyColor,
       button_label: buttonLabel,
       button_link: buttonLink,
     } = primary;
 
     const hasTitle = TextUtils.hasRichText(title);
     const hasSubtitle = TextUtils.hasRichText(subtitle);
-    const backgroundStyleClass = StyleUtils.getBackgroundStyle(backgroundStyle);
-    const titleColorClass = StyleUtils.getTitleColor(backgroundStyle);
+    const backgroundHeaderStyles = `${StyleUtils.getBackgroundColor(backgroundHeaderColor)} ${StyleUtils.getBackgroundStyle(backgroundHeaderStyle)}`;
+    let backgroundBodyStyles = `${StyleUtils.getBackgroundColor(backgroundBodyColor)} ${StyleUtils.getBackgroundStyle(backgroundBodyStyle)}`;
+    const titleColorStyle = StyleUtils.getTitleColor(backgroundHeaderColor);
     const hasPrimary = hasTitle || hasSubtitle || hasSeparationLine;
+    backgroundBodyStyles = buttonLabel ? backgroundBodyStyles : `${backgroundBodyStyles} "md:pb-20"`;
     return (
-      <div className={`mx-auto px-6 lg:px-28 text-center ${backgroundStyleClass}`}>
+      <div className="mx-auto text-center">
         {hasPrimary && (
           <div
-            className={`flex flex-col justify-between items-center py-10 px-6 container mx-auto lg:pt-20 lg:pb-28`}
+            className={`flex flex-col justify-between items-center py-10 px-12 lg:px-28 mx-auto lg:pt-20 lg:pb-28 ${backgroundHeaderStyles}`}
           >
             {TextUtils.hasRichText(hiddenTitle) && (
               <div className="hidden">{RichText.render(hiddenTitle, linkResolver)}</div>
             )}
             {hasTitle && (
               <div className="pb-5">
-                <span className={`font-bold text-xl lg:text-3xl ${titleColorClass} `}>
+                <span className={`font-bold text-xl lg:text-3xl ${titleColorStyle} `}>
                   {RichText.render(title, linkResolver)}
                 </span>
               </div>
@@ -52,9 +57,9 @@ class CardLinks extends Component {
             {hasSeparationLine && <div className="border-b-2 border-secondary w-28 pb-5"></div>}
           </div>
         )}
-        {this.renderCards()}
+        {this.renderCards(backgroundBodyStyles)}
         {buttonLabel && (
-          <div className="w-4/6 md:w-2/6 xl:w-1/5 mx-auto z-10 pb-10 md:pb-15 xl:pb-15">
+          <div className="w-4/6 md:w-2/6 xl:w-1/5 mx-auto z-10 pb-20 md:pb-15 xl:pb-15">
             <Button link={buttonLink} label={buttonLabel} style="filled" />
           </div>
         )}
@@ -62,12 +67,12 @@ class CardLinks extends Component {
     );
   }
 
-  renderCards() {
+  renderCards(backgroundBodyStyles) {
     const { items: cards } = this.props.slice;
     return (
-      <div className="flex flex-col h-auto relative -top-4 lg:-top-20 z-10  md:flex-row md:space-x-7 md:items-center text-primary-dark text-center">
+      <div className={`flex flex-col h-auto relative px-6 lg:px-28 -top-4 lg:-top-20 z-10 ${backgroundBodyStyles} md:flex-row md:space-x-7 md:items-center text-primary-dark text-center`}>
         {cards.map((card, index) => {
-          const { card_image, card_title, card_description, card_style, card_link } = card;
+          const { card_image, card_title, card_description, card_style, card_link, card_link_label } = card;
           const cardClass =
             card_style === CARD_STYLE.HIGHLIGHTED ? "md:py-16 lg:py-20" : "md:py-11";
           return (
@@ -76,10 +81,15 @@ class CardLinks extends Component {
                 className={`card hover_translate-y-2 h-full w-full flex flex-col items-center mb-10 md:mb-0 py-10 px-8 xl:max-w-1/5 ${cardClass}`}
               >
                 <Image image={card_image} />
-                <div className=" text-xl font-bold py-6 lg:text-2xl lg:pt-6 lg:pb-12">
+                <div className="text-xl font-bold py-6 lg:text-2xl lg:pt-6 lg:pb-12">
                   {RichText.render(card_title, linkResolver)}
                 </div>
                 <div>{card_description}</div>
+                {card_link_label && (
+                  <div className="font-bold pt-6 underline">
+                    {card_link_label}
+                  </div>
+                )}
               </div>
             </CustomLink>
           );
--- utils/styleUtils.js
+++ utils/styleUtils.js
@@ -1,33 +1,57 @@
-const STYLE = {
+const BACKGROUND_COLOR = {
   LIGHT: "light",
   DARK: "dark",
   WHITE: "white",
 };
 
+const BACKGROUND_STYLE = {
+  DOTS_1: "dots1",
+  DOTS_2: "dots2",
+  DOTS_3: "dots3",
+  BLUE_OVAL: "blue-oval",
+};
+
 export default class StyleUtils {
-  static getBackgroundStyle(style) {
-    switch (style) {
-      case STYLE.LIGHT:
+
+  static getBackgroundColor(color) {
+    switch (color) {
+      case BACKGROUND_COLOR.LIGHT:
         return "bg-primary-lighter";
-      case STYLE.DARK:
+      case BACKGROUND_COLOR.DARK:
         return "bg-primary-dark";
-      case STYLE.WHITE:
+      case BACKGROUND_COLOR.WHITE:
         return "bg-primary-white";
       default:
         return "bg-white";
     }
   }
-  static getTitleColor(style) {
-    return style === STYLE.DARK ? "text-secondary" : "text-primary-dark";
+
+  static getTitleColor(backgroundColor) {
+    return backgroundColor === BACKGROUND_COLOR.DARK ? "text-secondary" : "text-primary-dark";
   }
 
-  getTextColor(style) {
+  static getBackgroundStyle(style) {
     switch (style) {
-      case STYLE.LIGHT:
+      case BACKGROUND_STYLE.DOTS_1:
+        return "blue-oval-left-and-dots-right-bg";
+      case BACKGROUND_STYLE.DOTS_2:
+        return "dots2";
+      case BACKGROUND_STYLE.DOTS_3:
+        return "dots3";
+      case BACKGROUND_STYLE.BLUE_OVAL:
+        return "blue-top-right-oval-bg dots2";
+      default:
+        return "";
+    }
+  }
+
+  getTextColor(backgroundColor) {
+    switch (backgroundColor) {
+      case BACKGROUND_COLOR.LIGHT:
         return "text-primary-dark";
-      case STYLE.DARK:
+      case BACKGROUND_COLOR.DARK:
         return "text-white";
-      case STYLE.WHITE:
+      case BACKGROUND_COLOR.WHITE:
         return "text-primary font-medium";
       default:
         return "text-primary font-medium";

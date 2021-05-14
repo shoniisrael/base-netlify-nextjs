05ba87a80b71e901d22c7e13640ee258204d1532
Eduardo Lima G <eduarditolima@gmail.com>
2021-05-13 20:09:59 -0500

    Changes to adapt 4 cards

--- components/slices/cardLinks.js
+++ components/slices/cardLinks.js
@@ -15,6 +15,7 @@ class CardLinks extends Component {
       hidden_title: hiddenTitle,
       title,
       subtitle,
+      header_text_align: headerTextAlign,
       has_separation_line: hasSeparationLine,
       background_header_style: backgroundHeaderStyle,
       background_header_color: backgroundHeaderColor,
@@ -38,18 +39,23 @@ class CardLinks extends Component {
       hasSubtitle ||
       hasSeparationLine ||
       backgroundHeaderStyle !== BACKGROUND_STYLE.NONE;
-    const paddingBodyStyles = buttonLabel ? "" : "md:pb-20";
+    let paddingBodyStyles = buttonLabel ? "" : "md:pb-20";
     const bodyStyles =
       backgroundBodyStyle === BACKGROUND_STYLE.NONE ? "-top-4 lg:-top-20" : "md:pb-10";
     backgroundHeaderStyles =
       backgroundBodyStyle === BACKGROUND_STYLE.NONE
         ? `lg:pb-28 ${backgroundHeaderStyles}`
         : `lg:pb-12 ${backgroundHeaderStyles} `;
+    const alignItemsStyle = backgroundBodyStyle === BACKGROUND_STYLE.NONE ? "center" : "stretch";
+    paddingBodyStyles =
+      backgroundBodyStyle === BACKGROUND_STYLE.NONE
+        ? paddingBodyStyles
+        : `${paddingBodyStyles} pb-10`;
     return (
       <div className={`mx-auto text-center ${backgroundBodyStyles}`}>
         {hasPrimary && (
           <div
-            className={`flex flex-col justify-between items-center py-10 px-12 lg:px-28 mx-auto lg:pt-10  ${backgroundHeaderStyles}`}
+            className={`flex flex-col justify-between items-center text-${headerTextAlign} py-10 px-12 lg:px-28 mx-auto lg:pt-10  ${backgroundHeaderStyles}`}
           >
             {TextUtils.hasRichText(hiddenTitle) && (
               <div className="hidden">{RichText.render(hiddenTitle, linkResolver)}</div>
@@ -62,8 +68,8 @@ class CardLinks extends Component {
               </div>
             )}
             {hasSubtitle && (
-              <div className="w-5/6 md:w-3/6 lg:w-3/6 ">
-                <span className="text-center font-light">
+              <div className="w-5/6 md:w-4/6 ">
+                <span className={`text-${headerTextAlign} font-light`}>
                   {RichText.render(subtitle, linkResolver)}
                 </span>
               </div>
@@ -71,9 +77,9 @@ class CardLinks extends Component {
             {hasSeparationLine && <div className="border-b-2 border-secondary w-28 pb-5"></div>}
           </div>
         )}
-        {this.renderCards(bodyStyles, paddingBodyStyles)}
+        {this.renderCards(bodyStyles, paddingBodyStyles, alignItemsStyle)}
         {buttonLabel && (
-          <div className="w-4/6 md:w-2/6 xl:w-1/5 mx-auto z-10 pb-20 md:pb-15 xl:pb-15">
+          <div className="w-4/6 md:w-2/6 xl:w-1/5 mx-auto z-10 pb-10 md:pb-15 xl:pb-15">
             <Button link={buttonLink} label={buttonLabel} style="filled" />
           </div>
         )}
@@ -81,12 +87,15 @@ class CardLinks extends Component {
     );
   }
 
-  renderCards(bodyStyles, paddingBodyStyles) {
+  renderCards(bodyStyles, paddingBodyStyles, alignItemsStyle) {
     const { items: cards } = this.props.slice;
+    const cols = cards.length === 4 ? 2 : cards.length;
+    paddingBodyStyles = `${paddingBodyStyles} ${cols === 2 ? "lg:px-40" : "lg:px-28"}`;
+    alignItemsStyle = cols !== 2 ? alignItemsStyle : "stretch";
     return (
       <div className={`px-6 lg:px-28 z-10 ${paddingBodyStyles}`}>
         <div
-          className={`flex flex-col h-auto relative ${bodyStyles} md:flex-row md:space-x-7 md:items-center text-primary-dark text-center`}
+          className={`grid grid-cols-1 md:grid-cols-${cols} gap-y-10 h-auto relative ${bodyStyles} md:gap-x-7 md:items-${alignItemsStyle} text-primary-dark text-center`}
         >
           {cards.map((card, index) => {
             const {

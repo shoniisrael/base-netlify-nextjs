4d405be8d34bb36e0888f3716740408886031378
Eduardo Lima G <eduarditolima@gmail.com>
2021-05-14 15:20:30 -0500

    Separate the code into functions

--- components/slices/cardLinks.js
+++ components/slices/cardLinks.js
@@ -3,8 +3,8 @@ import { RichText } from "prismic-reactjs";
 import { linkResolver } from "../../prismic-configuration";
 import CustomLink from "../common/customLink";
 import Image from "../common/Image";
-import Button from "./../common/button";
-import ResponsiveBgImage from "./../common/responsiveBgImage";
+import Button from "../common/button";
+import ResponsiveBgImage from "../common/responsiveBgImage";
 import { CARD_STYLE } from "../../utils/constants";
 import TextUtils from "../../utils/text";
 import StyleUtils, { BACKGROUND_STYLE, TEXT_ALIGN } from "../../utils/styleUtils";
@@ -14,6 +14,27 @@ class CardLinks extends Component {
     const { slice, index } = this.props;
     const { primary } = slice;
     const {
+      background_image: backgroundImage,
+      background_body_style: backgroundBodyStyle,
+      background_body_color: backgroundBodyColor,
+    } = primary;
+
+    const backgroundBodyStyles = `${StyleUtils.getBackgroundColor(
+      backgroundBodyColor,
+    )} ${StyleUtils.getBackgroundStyle(backgroundBodyStyle)}`;
+
+    return (
+      <ResponsiveBgImage index={index} bgImage={backgroundImage} classes={backgroundBodyStyles}>
+        {this.renderHeader()}
+        {this.renderCards()}
+        {this.renderFooter()}
+      </ResponsiveBgImage>
+    );
+  }
+
+  renderHeader() {
+    const { primary } = this.props.slice;
+    const {
       hidden_title: hiddenTitle,
       title,
       title_color: titleColor,
@@ -21,13 +42,9 @@ class CardLinks extends Component {
       subtitle_color: subtitleColor,
       header_text_align: headerTextAlign,
       has_separation_line: hasSeparationLine,
-      background_image: backgroundImage,
       background_header_style: backgroundHeaderStyle,
       background_header_color: backgroundHeaderColor,
       background_body_style: backgroundBodyStyle,
-      background_body_color: backgroundBodyColor,
-      button_label: buttonLabel,
-      button_link: buttonLink,
     } = primary;
 
     const hasTitle = TextUtils.hasRichText(title);
@@ -36,96 +53,96 @@ class CardLinks extends Component {
       backgroundHeaderColor,
     )} ${StyleUtils.getBackgroundStyle(backgroundHeaderStyle)} 
     ${StyleUtils.getTextColor(subtitleColor, backgroundHeaderColor)}`;
-    const backgroundBodyStyles = `${StyleUtils.getBackgroundColor(
-      backgroundBodyColor,
-    )} ${StyleUtils.getBackgroundStyle(backgroundBodyStyle)}`;
+
     const titleColorStyle = StyleUtils.getTitleColor(titleColor, backgroundHeaderColor);
     const hasPrimary =
       hasTitle ||
       hasSubtitle ||
       hasSeparationLine ||
-      backgroundHeaderStyle !== BACKGROUND_STYLE.NONE;
-    let paddingBodyStyles = buttonLabel ? "" : "md:pb-20";
-    const bodyStyles =
-      backgroundBodyStyle === BACKGROUND_STYLE.NONE ? "-top-4 lg:-top-20" : "md:pb-10";
+      (backgroundHeaderStyle && backgroundHeaderStyle !== BACKGROUND_STYLE.NONE);
+
     backgroundHeaderStyles = `${backgroundHeaderStyles} ${
-      backgroundBodyStyle === BACKGROUND_STYLE.NONE ? "lg:pb-28" : "lg:pb-12"
+      headerTextAlign === TEXT_ALIGN.LEFT ? "items-start text-left" : "items-center text-center"
     }`;
     backgroundHeaderStyles = `${backgroundHeaderStyles} ${
-      headerTextAlign === TEXT_ALIGN.CENTER ? "items-center text-center" : "items-start text-left"
+      !backgroundBodyStyle || backgroundBodyStyle === BACKGROUND_STYLE.NONE
+        ? "lg:pb-28"
+        : "lg:pb-12"
     }`;
-    const alignItemsStyle = backgroundBodyStyle === BACKGROUND_STYLE.NONE ? "center" : "stretch";
-    paddingBodyStyles = `${paddingBodyStyles} ${
-      backgroundBodyStyle === BACKGROUND_STYLE.NONE ? "" : "pb-10"
-    }`;
+
     const titleWidthStyle = headerTextAlign === TEXT_ALIGN.LEFT ? "md:w-3/6" : "";
+
     return (
-      <ResponsiveBgImage index={index} bgImage={backgroundImage} classes={backgroundBodyStyles}>
-        {hasPrimary && (
-          <div
-            className={`flex flex-col justify-between py-10 px-12 lg:px-28 mx-auto lg:pt-10  ${backgroundHeaderStyles}`}
-          >
-            {TextUtils.hasRichText(hiddenTitle) && (
-              <div className="hidden">{RichText.render(hiddenTitle, linkResolver)}</div>
-            )}
-            {hasTitle && (
-              <div className={`pb-5 ${titleWidthStyle}`}>
-                <span className={`font-bold text-xl lg:text-3xl ${titleColorStyle} `}>
-                  {RichText.render(title, linkResolver)}
-                </span>
-              </div>
-            )}
-            {hasSubtitle && (
-              <div className="md:w-4/6 ">
-                <span className="font-light">{RichText.render(subtitle, linkResolver)}</span>
-              </div>
-            )}
-            {hasSeparationLine && <div className="border-b-2 border-secondary w-28 pb-5"></div>}
-          </div>
-        )}
-        {this.renderCards(bodyStyles, paddingBodyStyles, alignItemsStyle)}
-        {buttonLabel && (
-          <div className="w-4/6 md:w-2/6 xl:w-1/5 mx-auto z-10 pb-10 md:pb-15 xl:pb-15">
-            <Button link={buttonLink} label={buttonLabel} style="filled" />
-          </div>
-        )}
-      </ResponsiveBgImage>
+      hasPrimary && (
+        <div
+          className={`flex flex-col justify-between py-10 px-12 lg:px-28 mx-auto lg:pt-10  ${backgroundHeaderStyles}`}
+        >
+          {TextUtils.hasRichText(hiddenTitle) && (
+            <div className="hidden">{RichText.render(hiddenTitle, linkResolver)}</div>
+          )}
+          {hasTitle && (
+            <div className={`pb-5 ${titleWidthStyle}`}>
+              <span className={`font-bold text-xl lg:text-3xl ${titleColorStyle} `}>
+                {RichText.render(title, linkResolver)}
+              </span>
+            </div>
+          )}
+          {hasSubtitle && (
+            <div className="md:w-4/6 ">
+              <span className="font-light">{RichText.render(subtitle, linkResolver)}</span>
+            </div>
+          )}
+          {hasSeparationLine && <div className="border-b-2 border-secondary w-28 pb-5"></div>}
+        </div>
+      )
     );
   }
 
-  renderCards(bodyStyles, paddingBodyStyles, alignItemsStyle) {
-    const { items: cards } = this.props.slice;
+  renderCards() {
+    const { items: cards, primary } = this.props.slice;
+    const { background_body_style: backgroundBodyStyle, button_label: buttonLabel } = primary;
+    let alignItemsStyle =
+      !backgroundBodyStyle || backgroundBodyStyle === BACKGROUND_STYLE.NONE ? "center" : "stretch";
+    let paddingBodyStyles = buttonLabel ? "" : "md:pb-20";
+    paddingBodyStyles = `${paddingBodyStyles} ${
+      !backgroundBodyStyle || backgroundBodyStyle === BACKGROUND_STYLE.NONE ? "" : "pb-10"
+    }`;
+    const bodyStyles =
+      !backgroundBodyStyle || backgroundBodyStyle === BACKGROUND_STYLE.NONE
+        ? "-top-4 lg:-top-20"
+        : "md:pb-10";
+
     const cols = cards.length === 4 ? 2 : cards.length;
-    paddingBodyStyles = `${paddingBodyStyles} ${cols === 2 ? "lg:px-40" : "lg:px-28"}`;
+    paddingBodyStyles = `${paddingBodyStyles} ${cols === 2 ? "lg:px-40" : "lg:px-20"}`;
     alignItemsStyle = cols !== 2 ? alignItemsStyle : "stretch";
+
     return (
       <div className={`px-6 lg:px-28 z-10 ${paddingBodyStyles}`}>
         <div
-          className={`grid grid-cols-1 md:grid-cols-${cols} gap-y-10 h-auto relative ${bodyStyles} md:gap-x-7 md:items-${alignItemsStyle} text-primary-dark text-center`}
+          className={`grid grid-cols-1 md:grid-cols-${cols} gap-y-10 h-auto relative ${bodyStyles} md:gap-x-8 md:items-${alignItemsStyle} text-primary-dark text-center`}
         >
           {cards.map((card, index) => {
             const {
-              card_image,
-              card_title,
-              card_description,
-              card_style,
-              card_link,
-              card_link_label,
+              card_image: cardImage,
+              card_title: cardTitle,
+              card_description: cardDescription,
+              card_style: cardStyle,
+              card_link: cardLink,
+              card_link_label: cardLinkLabel,
             } = card;
-            const cardClass = card_style === CARD_STYLE.HIGHLIGHTED ? "md:py-16" : "md:py-11";
+            const cardClass =
+              cardStyle === CARD_STYLE.HIGHLIGHTED ? "md:py-16 lg:py-20" : "md:py-11";
             return (
-              <CustomLink key={index} link={card_link}>
+              <CustomLink key={index} link={cardLink}>
                 <div
                   className={`card hover_translate-y-2 h-full w-full flex flex-col items-center mb-10 md:mb-0 py-10 px-8 xl:max-w-1/5 ${cardClass}`}
                 >
-                  <Image image={card_image} />
+                  <Image image={cardImage} />
                   <div className="text-xl font-bold py-6 lg:text-2xl lg:pt-6 lg:pb-12">
-                    {RichText.render(card_title, linkResolver)}
+                    {RichText.render(cardTitle, linkResolver)}
                   </div>
-                  <div>{card_description}</div>
-                  {card_link_label && (
-                    <div className="font-bold pt-6 underline">{card_link_label}</div>
-                  )}
+                  <div>{cardDescription}</div>
+                  {cardLinkLabel && <div className="font-bold pt-6 underline">{cardLinkLabel}</div>}
                 </div>
               </CustomLink>
             );
@@ -134,6 +151,17 @@ class CardLinks extends Component {
       </div>
     );
   }
-}
 
+  renderFooter() {
+    const { primary } = this.props.slice;
+    const { button_label: buttonLabel, button_link: buttonLink } = primary;
+    return (
+      buttonLabel && (
+        <div className="w-4/6 md:w-2/6 xl:w-1/5 mx-auto z-10 pb-10 md:pb-15 xl:pb-15">
+          <Button link={buttonLink} label={buttonLabel} style="filled" />
+        </div>
+      )
+    );
+  }
+}
 export default CardLinks;

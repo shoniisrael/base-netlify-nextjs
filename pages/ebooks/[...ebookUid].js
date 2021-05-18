0e8d2cb8dc9e109b8ae2dd27f896cfa0d1b8121e
Eduardo Lima G <eduarditolima@gmail.com>
2021-05-18 15:27:43 -0500

    Update return fallback=blocking depending the ENABLE_PREVIEW_MODE,  inside getStaticPaths methods

--- pages/ebooks/[...ebookUid].js
+++ pages/ebooks/[...ebookUid].js
@@ -75,7 +75,7 @@ export async function getStaticPaths(context) {
     return { params: { ebookUid: ebook.uid.split("_") } };
   });
   return {
-    fallback: context.preview ? "blocking" : false,
+    fallback: process.env.ENABLE_PREVIEW_MODE === "true" ? "blocking" : false,
     paths,
   };
 }

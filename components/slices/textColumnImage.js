764279c871ce3a7c101b7cdd9bac2bc77240b304
Jonathan Franco <jonas150397@gmail.com>
2021-05-18 15:58:04 -0500

    update-text-column-image-cards

--- components/slices/textColumnImage.js
+++ components/slices/textColumnImage.js
@@ -3,12 +3,51 @@ import React, { Component } from "react";
 import { linkResolver } from "../../prismic-configuration";
 import ResponsiveImage from "../common/responsiveImage";
 
-class TextColumnImage extends Component {
-  renderGrid() {
-    const { title, description, image } = this.props.slice.primary;
+const BG_STYLE = {
+  DOTS_7: "dots7",
+};
 
+const CARD_STYLE = {
+  DARK: "dark",
+  LIGHT: "light",
+};
+
+class TextColumnImage extends Component {
+  getBackgroundClasses(bgStyle) {
+    switch (bgStyle) {
+      case BG_STYLE.DOTS_7:
+        return "dots7";
+      default:
+        return "";
+    }
+  }
+
+  getGridColumns(hasImage) {
+    if (hasImage) return "justify-items-center grid-cols-1 md:grid-cols-2";
+    return "";
+  }
+
+  getCardStyleClasses(cardStyle) {
+    switch (cardStyle) {
+      case CARD_STYLE.LIGHT:
+        return {
+          background: "bg-primary-lightest rounded-md shadow-md",
+          titleColor: "font-bold text-primary-blue",
+          descriptionColor: "text-primary",
+        };
+      case CARD_STYLE.DARK:
+        return {
+          titleColor: "font-bold",
+        };
+      default:
+        return "";
+    }
+  }
+
+  renderImageGridHeader() {
+    const { title, description, image } = this.props.slice.primary;
     return (
-      <div className="grid grid-cols-1 justify-items-center md:grid-cols-2">
+      <>
         <div className="p-7 flex flex-col justify-center">
           <div className="text-secondary font-bold self-end text-xl md:text-4xl lg:text5x1">
             {RichText.render(title, linkResolver)}
@@ -17,35 +56,68 @@ class TextColumnImage extends Component {
             {RichText.render(description, linkResolver)}
           </div>
         </div>
-        <div className="mt-2 md:-mt-6">
-          <ResponsiveImage image={image} sizes="(min-width:768) 100vw, 30vw" />
+        <ResponsiveImage image={image} sizes="(min-width:768) 100vw, 30vw" />
+      </>
+    );
+  }
+
+  renderNormalGridHeader() {
+    const { title, description } = this.props.slice.primary;
+    return (
+      <>
+        <div className="p-7 text-center mt-5 sm:py-10 sm:px-20 md:py-20">
+          <div className="font-bold text-lg md:text-2xl lg:text-3xl">
+            {RichText.render(title, linkResolver)}
+          </div>
+          <div className="mt-7 md:px-9 lg:px-32 xl:px-60">
+            {RichText.render(description, linkResolver)}
+          </div>
         </div>
+      </>
+    );
+  }
+
+  renderGrid() {
+    const { image } = this.props.slice.primary;
+    const hasImage = !!Object.values(image).length;
+    const columns = this.getGridColumns(hasImage);
+    return (
+      <div className={`grid ${columns}`}>
+        {hasImage && this.renderImageGridHeader()}
+        {!hasImage && this.renderNormalGridHeader()}
       </div>
     );
   }
 
   renderCards() {
-    const { items } = this.props.slice;
+    const { primary, items } = this.props.slice;
+    const { card_style: cardStyleBg } = primary;
+    const cardStyle = this.getCardStyleClasses(cardStyleBg);
     return (
-      <div className="flex flex-wrap justify-evenly mx-5 mt-5">
-        {items.map((item, index) => this.renderLightCard(item, index))}
+      <div className="flex flex-wrap justify-center mx-5 mt-5">
+        {items.map((item, index) => this.renderStyleCard(item, index, cardStyle))}
       </div>
     );
   }
 
-  renderLightCard(card, index) {
+  renderStyleCard(card, index, cardStyle) {
+    const { background, titleColor, descriptionColor } = cardStyle;
     const { card_title: cardTitle, card_description: cardDescription } = card;
     return (
-      <div key={index} className="w-80 mx-5 my-5 py-2 px-4">
-        <div className="font-bold mb-8 text-xl">{RichText.render(cardTitle, linkResolver)}</div>
-        {RichText.render(cardDescription, linkResolver)}
+      <div key={index} className={`w-72 mx-5 my-5 py-6 px-6 ${background}`}>
+        <div className={`mb-8 text-lg ${titleColor}`}>
+          {RichText.render(cardTitle, linkResolver)}
+        </div>
+        <div className={descriptionColor}>{RichText.render(cardDescription, linkResolver)}</div>
       </div>
     );
   }
 
   render() {
+    const { background_style: backgroundStyle } = this.props.slice.primary;
+    const bgStyle = this.getBackgroundClasses(backgroundStyle);
     return (
-      <div className="container mx-auto w-full bg-primary-dark ">
+      <div className={`container mx-auto w-full bg-primary-dark my-5${bgStyle}`}>
         {this.renderGrid()}
         {this.renderCards()}
       </div>
